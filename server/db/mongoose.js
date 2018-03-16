179f7e6f8bb5a34499491c812e60e9702d911784
DWebSoft <dwebsoftguy@gmail.com>
2018-03-16 19:21:03 +0530

    Setup Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,14 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/TodoApp');
+
+var connectPath;
+if(process.env.PORT){
+    connectPath = "mongodb://durgesh:durgesh7$$@ds115579.mlab.com:15579/durgesh-todo";
+}else{
+    connectPath = "mongodb://localhost/TodoApp";
+}
+mongoose.connect(connectPath);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
