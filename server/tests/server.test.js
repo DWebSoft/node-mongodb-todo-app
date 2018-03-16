8336f283d04a4a70461d747bd9bf1b947cbf7e67
DWebSoft <dwebsoftguy@gmail.com>
2018-03-16 15:53:26 +0530

    Add tests for GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,15 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+var todos = [
+    { title: 'First todo' },
+    { title: 'Second todo' }
+];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -23,7 +30,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 //Check if Todo is stored in db
-                Todo.find().then((todos)=>{
+                Todo.find({title}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].title).toBe(title);
                     done();
@@ -41,9 +48,21 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((err) => done(err));
             })
     });    
+});
+
+describe('GET /todos', () =>{
+    it('should return all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
