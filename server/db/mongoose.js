0be326dad92062d4d616171da855b505852dc0d7
DWebSoft <dwebsoftguy@gmail.com>
2018-03-16 21:00:50 +0530

    Heroku mlab connection issue

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,11 +4,18 @@ mongoose.Promise = global.Promise;
 
 var connectPath;
 if(process.env.PORT){
-    connectPath = "mongodb://durgesh:durgesh7$$@ds115579.mlab.com:15579/durgesh-todo";
+    connectPath = "mongodb://durgesh@ds115579.mlab.com:15579/durgesh-todo";
+    creds = {
+        auth: {
+            user: 'durgesh',
+            password: 'durgesh7$$'
+        }
+    }
 }else{
     connectPath = "mongodb://localhost/TodoApp";
+    creds = {}
 }
-mongoose.connect(connectPath);
+mongoose.connect(connectPath, creds);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
