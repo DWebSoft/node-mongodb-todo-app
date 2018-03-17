3ffc88b2a9111f25758dafce7a030a9447f0a56b
DWebSoft <dwebsoftguy@gmail.com>
2018-03-17 17:53:25 +0530

    Added tests for PATCH /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -7,7 +7,12 @@ const {Todo} = require('./../models/todo');
 
 var todos = [
     { _id: new ObjectID(), title: 'First todo' },
-    { _id: new ObjectID(), title: 'Second todo' }
+    { 
+        _id: new ObjectID(),
+        title: 'Second todo',
+        completed: true,
+        completedAt: 1213
+    }
 ];
 
 beforeEach((done) => {
@@ -128,6 +133,42 @@ describe('DELETE /todos/:id', () => {
         request(app)
             .delete('/todos/123')
             .expect(404)
+            .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    var hexId = todos[1]._id.toHexString();
+    var title = 'Third Todo';
+    it('should update todo', (done) => {
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                title,
+                completed: true
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.title).toBe(title);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).not.toBe(null);
+            })
+            .end(done);
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                title,
+                completed: false
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.title).toBe(title);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBeFalsy();
+            })
             .end(done);
     });
 });
